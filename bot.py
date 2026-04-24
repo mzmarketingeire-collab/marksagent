704d1bdbbd99d97dd3a538e22e79989dde266613
Mark Hoare <markhoare@Marks-MacBook-Air.local>
2026-04-25 01:36:31 +1200

    add discord bot

---
+++ bot.py
@@ -0,0 +1,23 @@
+import discord
+import os
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.lower().startswith("hello"):
+        await message.channel.send("Hey, I'm alive!")
+
+client.run(TOKEN)
